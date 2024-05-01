13aa1991cf4f00a1c536a1514ee5411b2e6b5026
Atul Kulkarni <atul.c.kulkarni@outlook.com>
2024-05-01 21:40:13 +0530

    support to reverse contents based on file name

--- src/main/java/com/sample/FileProcessor.java
+++ src/main/java/com/sample/FileProcessor.java
@@ -2,6 +2,9 @@ package com.sample;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -9,6 +12,28 @@ import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 
 public class FileProcessor {
+
+  public void reverseContents(final String inputFileName, final String outputFileName)
+      throws IOException {
+    InputStream inputStream = getInputStream(inputFileName);
+    OutputStream outputStream = getOutStream(outputFileName);
+    reverseContents(inputStream, outputStream);
+  }
+
+  public InputStream getInputStream(String inputFileName) throws FileNotFoundException {
+    return new FileInputStream(inputFileName);
+  }
+
+  public OutputStream getOutStream(String outputFileName) throws FileNotFoundException {
+    return new FileOutputStream(outputFileName);
+  }
+
+  public void reverseContents(final InputStream inputStream, final OutputStream outputStream)
+      throws IOException {
+    String inputFileContents = readInputFile(inputStream);
+    String reverseContent = reverseContent(inputFileContents);
+    writeOutputFile(outputStream, reverseContent);
+  }
 
   public String readInputFile(final InputStream inputStream) throws IOException {
     StringBuilder content = new StringBuilder();
--- src/test/java/com/sample/FileProcessorTest.java
+++ src/test/java/com/sample/FileProcessorTest.java
@@ -1,19 +1,23 @@
 package com.sample;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.verify;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
+@ExtendWith(MockitoExtension.class)
 public class FileProcessorTest {
 
   @Test
   void testReadInputFile() throws IOException {
-
     // Arrange
     InputStream inputStream = new ByteArrayInputStream("ABC".getBytes());
     FileProcessor fileProcessor = new FileProcessor();
@@ -44,5 +48,37 @@ public class FileProcessorTest {
 
     // Assert
     assertEquals(reversedContent, outputStream.toString());
+  }
+
+  @Test
+  void testReverseContentBasedOnStreams() throws IOException {
+    // Arrange
+    InputStream inputStream = new ByteArrayInputStream("ABC".getBytes());
+    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+    FileProcessor fileProcessor = new FileProcessor();
+
+    // Act
+    fileProcessor.reverseContents(inputStream, outputStream);
+
+    // Assert
+    assertEquals("CBA", outputStream.toString());
+  }
+
+  @Test
+  void testReverseContentBasedOnFileNames() throws IOException {
+    // Arrange
+    InputStream inputStream = new ByteArrayInputStream("ABC".getBytes());
+    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+    FileProcessor fileProcessor = Mockito.spy(new FileProcessor());
+    doReturn(inputStream).when(fileProcessor).getInputStream(Mockito.anyString());
+    doReturn(outputStream).when(fileProcessor).getOutStream(Mockito.anyString());
+
+    // Act
+    fileProcessor.reverseContents("input.txt", "output.txt");
+
+    // Assert
+    assertEquals("CBA", outputStream.toString());
+    verify(fileProcessor).getInputStream("input.txt");
+    verify(fileProcessor).getOutStream("output.txt");
   }
 }
